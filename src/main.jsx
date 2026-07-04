29420cbbde2144359d7dc9ae1ff7324799db9d09
Agentcore IA <agentcore.ia@gmail.com>
2026-07-04 17:47:05 -0300

    Disable automatic menu translation

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+document.documentElement.lang = 'es'
+document.documentElement.translate = false
+document.documentElement.classList.add('notranslate')
+document.body.translate = false
+document.body.classList.add('notranslate')
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
